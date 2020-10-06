c7a292a5944cf2e5aaaa9a1a3a972b1fab457c1a
Ethan <eleetyson@gmail.com>
2020-10-06 10:17:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// remove the <main> with id 'main'
+main.remove('main');
+
+// set newHeader equal to an <h1> with id 'victory' and a message
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Ethan is the champion';
+document.body.appendChild(newHeader);
