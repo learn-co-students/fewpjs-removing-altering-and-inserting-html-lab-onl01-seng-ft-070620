6f7bc64cd0c9048cb9bae448473ba2963e296508
Amanda-Katherine <walker.akw@gmail.com>
2020-10-07 21:08:18 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ document.querySelector("main").remove()
 let newHeader = document.createElement("h1");
 newHeader.id = "victory"
 newHeader.innerHTML = "Cricket is the champion"
+newHeader.className = "cats"
